04885eacd59e57cf922258917a04e7a7ae90f611
Romina <108244092+rbf123@users.noreply.github.com.>
2024-04-24 14:00:22 -0500

    fixed blogs, added paginator, added padding and styling

--- personal_portfolioproject/blog/views.py
+++ personal_portfolioproject/blog/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Blog
+from django.core.paginator import Paginator
 
 # Create your views here.
+#def all_blogs(request):
+#    blogs = Blog.objects.order_by('-date')[:10]
+#   return render(request, 'blog/all_blogs.html', {'blogs':blogs})
+
 def all_blogs(request):
-    blogs = Blog.objects.order_by('-date')[:10]
-    return render(request, 'blog/all_blogs.html', {'blogs':blogs})
+    blogs_list = Blog.objects.order_by('-date')
+    paginator = Paginator(blogs_list, 10) # Show 10 blogs per page
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'blog/all_blogs.html', {'page_obj': page_obj})
 
 def detail(request, blog_id):
     blog = get_object_or_404(Blog, pk=blog_id)
